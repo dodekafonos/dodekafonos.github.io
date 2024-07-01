d31b70b33b41267774144a33a0510f7a40106bf3
Jonas Bueno <jonas.bueno@opt.com.br>
2024-07-01 19:08:46 -0300

    :tada: Initial commit: reacreating my portfolio from scratch.

--- README.md
+++
@@ -1,7 +0,0 @@
-# Jonas' corner
-
-🚧 Under construction. 🚧
-
-Reserved little corner in the web to publish my notes and articles and display my projects. 
-
-
---
+++ portfolio/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Hello! My name is Jonas.</h1>
+      <h2>This is my Portfolio.</h2>
+
+    </>
+  )
+}
+
+export default App
